d48bcf4495f23fb96dcd2bb702e8e4199ff0f364
Daniel <danielloza1108@gmail.com>
2020-12-22 01:29:32 +0100

    Add jsp and controller for donations list

--- src/main/java/pl/coderslab/charity/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/controllers/DonationController.java
@@ -1,8 +1,16 @@
 package pl.coderslab.charity.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class DonationController {
 
+    @GetMapping("/dontations")
+    public String donations(Model model){
+        return "donations"
+    }
 }
